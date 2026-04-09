f1dd53b54a9597ce37a346cf9eb71f832706e866
Abhinay Mahato <153537968+abhinay2708@users.noreply.github.com>
2026-04-09 09:55:46 +0530

    Create 48. Multiply Strings(43).py
    Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
    
    Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
    
     
    
    Example 1:
    
    Input: num1 = "2", num2 = "3"
    Output: "6"
    Example 2:
    
    Input: num1 = "123", num2 = "456"
    Output: "56088"
     
    
    Constraints:
    
    1 <= num1.length, num2.length <= 200
    num1 and num2 consist of digits only.
    Both num1 and num2 do not contain any leading zero, except the number 0 itself.

---
+++ LeetCode/48. Multiply Strings(43).py
@@ -0,0 +1,20 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        res = [0] * (len(num1) + len(num2))
+        
+        for i in range(len(num1) - 1, -1, -1):
+            for j in range(len(num2) - 1, -1, -1):
+                mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+                p1, p2 = i + j, i + j + 1
+                total = mul + res[p2]
+                res[p2] = total % 10
+                res[p1] += total // 10
+        
+        start = 0
+        while start < len(res) and res[start] == 0:
+            start += 1
+            
+        return "".join(map(str, res[start:]))
