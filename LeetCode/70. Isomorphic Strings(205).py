cff01d813528d44314f644fe1e9ff60f995b32d0
Abhinay Mahato <153537968+abhinay2708@users.noreply.github.com>
2026-05-01 21:53:10 +0530

    Create 70. Isomorphic Strings(205).py
    Given two strings s and t, determine if they are isomorphic.
    
    Two strings s and t are isomorphic if the characters in s can be replaced to get t.
    
    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
    
     
    
    Example 1:
    
    Input: s = "egg", t = "add"
    
    Output: true
    
    Explanation:
    
    The strings s and t can be made identical by:
    
    Mapping 'e' to 'a'.
    Mapping 'g' to 'd'.
    Example 2:
    
    Input: s = "f11", t = "b23"
    
    Output: false
    
    Explanation:
    
    The strings s and t can not be made identical as '1' needs to be mapped to both '2' and '3'.
    
    Example 3:
    
    Input: s = "paper", t = "title"
    
    Output: true
    
     
    
    Constraints:
    
    1 <= s.length <= 5 * 104
    t.length == s.length
    s and t consist of any valid ascii character.

---
+++ LeetCode/70. Isomorphic Strings(205).py
@@ -0,0 +1,21 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        d1={}
+        d2={}
+
+        for i in range(len(s)):
+            if s[i] in d1:
+                if d1[s[i]]!=t[i]:
+                    return False
+            else:
+                d1[s[i]]=t[i]
+            
+            if t[i] in d2:
+                if d2[t[i]]!=s[i]:
+                    return False
+            else:
+                d2[t[i]]=s[i]
+        return True
+
